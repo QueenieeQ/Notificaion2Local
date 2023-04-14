341fdb61d0c01abeb415b1c4602f9276fb43d738
QueenieeQ <xuanquyhnvn@gmail.com>
2023-04-15 00:12:08 +0700

    noti

--- Notification2Local/ViewController.swift
+++ Notification2Local/ViewController.swift
@@ -42,11 +42,37 @@ class ViewController: UIViewController {
                 content.title = title
                 content.body = message
                 
+                let dateComp = Calendar.current.dateComponents(
+                    [.year, .month, .day, .hour, .minute],
+                    from: date
+                )
+                let trigger = UNCalendarNotificationTrigger(
+                    dateMatching: dateComp,
+                    repeats: false
+                )
                 
+                let request = UNNotificationRequest(
+                    identifier: UUID().uuidString,
+                    content: content,
+                    trigger: trigger
+                )
+                self.notificationCenter.add(request) {(error) in
+                    if(error != nil){
+                        print("Error" + error.debugDescription)
+                        return
+                    }
+                }
+                let ac = UIAlertController(title: "Notification Scheduled", message: "At " + formattedDate(date: date), preferredStyle: .alert)
             }else{
                 
             }
     }
+        
+        func formattedDate(date: Date) -> String{
+            let formatter = DateFormatter()
+            formatter.dateFormat = "dd MM yy HH:mm"
+            return formatter.string(from: date)
+        }
     
     }
 }
